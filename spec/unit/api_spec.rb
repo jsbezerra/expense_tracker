471ed9d06840ce59055fc3877bd2f05f8dba2c75
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-09-21 22:17:46 -0300

    test: failing post /expenses

--- spec/unit/api_spec.rb
+++ spec/unit/api_spec.rb
@@ -14,11 +14,10 @@ module ExpenseTracker
     end
 
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+    let(:expense) { { 'some' => 'data' } }
 
     describe 'POST /expenses' do
       context 'when the expense is successfully recorded' do
-
-        let(:expense) { { 'some' => 'data' } }
 
         before do
           allow(ledger).to receive(:record).with(expense).and_return(RecordResult.new(true, 417, nil))
@@ -37,8 +36,22 @@ module ExpenseTracker
       end
 
       context 'when the expense fails validation' do
-        it 'returns an error message'
-        it 'responds with a 422 (Unprocessable entity)'
+
+        before do
+          allow(ledger).to receive(:record).with(expense).and_return(RecordResult.new(false, 417, 'Expense incomplete'))
+        end
+
+        it 'returns an error message' do
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('error' => 'Expense incomplete')
+        end
+
+        it 'responds with a 422 (Unprocessable entity)' do
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(422)
+        end
       end
     end
   end
