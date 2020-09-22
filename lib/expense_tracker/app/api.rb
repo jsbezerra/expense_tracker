4cc96d560aa3c8e1d3fadc5bd86c3682a0dcb76b
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-09-21 22:20:28 -0300

    feat: make post /expenses pass the failure case

--- lib/expense_tracker/app/api.rb
+++ lib/expense_tracker/app/api.rb
@@ -11,8 +11,14 @@ module ExpenseTracker
 
     post '/expenses' do
       expense = JSON.parse(request.body.read)
-      result = @ledger.record(expense)
-      JSON.generate('expense_id' => result.expense_id)
+      result  = @ledger.record(expense)
+
+      if result.success?
+        JSON.generate('expense_id' => result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end
     end
 
     get '/expenses/:date' do
