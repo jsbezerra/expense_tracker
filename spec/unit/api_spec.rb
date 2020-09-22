8e388c30f7f0c34a817e1629445f51aefa2bb241
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-09-21 21:14:43 -0300

    chore: unit tests for the post /expenses

---
+++ spec/unit/api_spec.rb
@@ -0,0 +1,17 @@
+require 'expense_tracker/app/api'
+
+module ExpenseTracker
+  describe API do
+    describe 'POST /expenses' do
+      context 'when the expense is successfully recorded' do
+        it 'returns the expense id'
+        it 'responds with a 200 (OK)'
+      end
+
+      context 'when the expense fails validation' do
+        it 'returns an error message'
+        it 'responds with a 422 (Unprocessable entity)'
+      end
+    end
+  end
+end
