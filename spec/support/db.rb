37874ee6b24c9a7463df9753ffb9d08fbfe756cb
Jonas S. Bezerra <jonassantosbezerra@outlook.com>
2021-08-03 20:29:40 -0300

    feat: log SQL queries executed in the integration tests

--- spec/support/db.rb
+++ spec/support/db.rb
@@ -3,9 +3,15 @@ RSpec.configure do |config|
     Sequel.extension :migration
     Sequel::Migrator.run DB, 'db/migrations'
     DB[:expenses].truncate
+
+    FileUtils.mkdir_p('log')
+    require 'logger'
+    DB.loggers << Logger.new('log/sequel.log')
   end
 
   config.around(:example, :db) do |example|
+    DB.log_info("Starting example: #{example.full_description}")
     DB.transaction(rollback: :always) { example.run }
+    DB.log_info("Ending example: #{example.full_description}")
   end
 end
